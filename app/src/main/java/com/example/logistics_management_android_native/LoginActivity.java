7487f6980f2d8b700a62b856df0326cffc9fa97f
Patricio Plem <patorafael.2000@gmail.com>
2025-04-07 11:32:28 -0300

    Login UI

---
+++ app/src/main/java/com/example/logistics_management_android_native/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.example.logistics_management_android_native;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    private TextView forgotPasswordText;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        usernameEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString().trim();
+            String password = passwordEditText.getText().toString().trim();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
+            } else {
+                // Aquí podrías verificar los datos o conectarte a un backend
+
+                Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
